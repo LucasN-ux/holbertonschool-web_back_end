e833b4a1013d0a393cb3bf903a2c6d56940a1905
LucasN-ux <lucas.n230896@gmail.com>
2026-04-06 18:41:33 +0200

    feat: Returns the sum of a list of floats

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+def sum_list(input_list: list) -> float:
+    """Returns the sum of a list of floats.
+
+    Args:
+        input_list: A list of floats to be summed.
+
+    Returns:
+        The sum of the list of floats.
+    """
+    return sum(input_list)
