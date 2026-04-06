e1287981cc2289f44163929ee24646cfe383b887
LucasN-ux <lucas.n230896@gmail.com>
2026-04-06 19:33:29 +0200

    feat: Returns a list of tuples with the length of each element in a list.

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from typing import Sequence, List, Tuple, Iterable
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Returns a list of tuples with the length of each element in a list.
+
+    Args:
+        lst: A list of sequences to be measured.
+
+    Returns:
+        A list of tuples with the length of each element in the list.
+    """
+    return [(i, len(i)) for i in lst]
