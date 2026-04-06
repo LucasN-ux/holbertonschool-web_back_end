6ced4264f5efa85e8b96b5a0e31205345fada361
LucasN-ux <lucas.n230896@gmail.com>
2026-04-06 18:17:57 +0200

    feat: retrun The floor of the float.

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+def floor(n: float) -> int:
+    """Returns the floor of a float.
+
+    Args:
+        n: The float to be floored.
+
+    Returns:
+        The floor of the float.
+    """
+    return int(n)
