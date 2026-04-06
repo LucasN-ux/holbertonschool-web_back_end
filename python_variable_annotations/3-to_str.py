9dff73ad1858654341a9309f5572720da8ee5bc2
LucasN-ux <lucas.n230896@gmail.com>
2026-04-06 18:25:13 +0200

    feat: Returns the string representation of a float.

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+def to_str(n: float) -> str:
+    """Returns the string representation of a float.
+
+    Args:
+        n: The float to be converted to a string.
+
+    Returns:
+        The string representation of the float.
+    """
+    return str(n)
