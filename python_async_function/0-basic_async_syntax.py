21ffbf3348c827e4957254aa8c565d132e466c88
LucasN-ux <lucas.n230896@gmail.com>
2026-04-07 11:01:40 +0200

    feat: Wait for a random delay between 0 and max_delay (inclusive) seconds.

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Wait for a random delay between 0 and max_delay (inclusive) seconds.
+
+    Args:
+        max_delay (int): The maximum number of seconds to wait. Default is 10.
+
+    Returns:
+        float: The actual delay time.
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
