d3ec7594d9dddaf1b1159e97f575ee5495bb1deb
LucasN-ux <lucas.n230896@gmail.com>
2026-04-08 13:27:48 +0200

    fix: Return a tuple with a string and the square of a number as a float.

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python3
-"""Defines a function to_kv that takes a string k
-and an int or float v and returns a tuple.
-"""
+"""Module for creating a key-value tuple."""
+
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: int | float) -> tuple:
-    """Returns a tuple of the key and value.
-
-    Args:
-        k: The key to be used in the tuple.
-        v: The value to be used in the tuple.
-    Returns:
-        A tuple of the key and value.
-    """
-    return (k, v * v)
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Return a tuple with a string and the square of a number as a float."""
+    return (k, float(v ** 2))
