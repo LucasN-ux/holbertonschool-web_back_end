3211afda3a713096f51c8ad98737c54490d62586
LucasN-ux <lucas.n230896@gmail.com>
2026-05-05 14:52:05 +0200

    feat: implement getListStudentsIds function to return a list of student objects

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudentsIds() {
+  const guillaume = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const james = { id: 2, firstName: 'James', location: 'Columbia' };
+  const alicia = { id: 5, firstName: 'Alicia', location: 'Paris' };
+  
+  return [guillaume, james, alicia]
+}
