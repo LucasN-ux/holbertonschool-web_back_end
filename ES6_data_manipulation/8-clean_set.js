4435d246a3007aeba04becfa6d4252b05a59a4c6
LucasN-ux <lucas.n230896@gmail.com>
2026-05-06 14:34:10 +0200

    feat: implement cleanset function to filter elements of a Set by a starting string

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default function cleanset(set, startString) {
+    if (startString.length === 0) {
+        return;
+    }
+    const string = [];
+    for (const element of set) {
+        if (element.startsWith(startString)) {
+            string.push(element.slice(startString.length));
+        }
+    }
+    return string.join("-");
+}
