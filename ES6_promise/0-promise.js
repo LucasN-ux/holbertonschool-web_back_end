c40adc53aee911198654510f18297561613371af
LucasN-ux <lucas.n230896@gmail.com>
2026-05-06 15:16:00 +0200

    fix: update ES6_promise/0-promise.js to return a resolved promise with value true

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -2,5 +2,8 @@ export default function getResponsefromAPI() {
     const promise = new Promise((resolve, reject) => {
         resolve(true);
     });
+    promise.then((response) => {
+        return response;
+    });
     return promise;
 }
