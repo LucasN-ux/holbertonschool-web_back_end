b32447f5a38225c808ec727a4cf90ea24e324e0d
LucasN-ux <lucas.n230896@gmail.com>
2026-05-12 11:47:39 +0200

    fix: remove unnecessary trim from input name output

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.toString().trim()}`);
+  console.log(`Your name is: ${data.toString()}`);
 });
 
 process.stdin.on('close', () => {
