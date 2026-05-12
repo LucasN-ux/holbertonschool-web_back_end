915a9be170d3d60707a2da1fb97771eac90b5f6c
LucasN-ux <lucas.n230896@gmail.com>
2026-05-12 11:44:02 +0200

    fix: standardize string quotes and simplify close message logic

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,9 @@
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on("data", (data) => {
+process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data.toString().trim()}`);
 });
 
-process.stdin.on("close", () => {
-  if (!process.stdin.isTTY) {
-    console.log("This important software is now closing");
-  }
+process.stdin.on('close', () => {
+  console.log('This important software is now closing');
 });
