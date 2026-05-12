8c60b2ce57a83cb50adae3d5fce3c91847d6c39c
LucasN-ux <lucas.n230896@gmail.com>
2026-05-12 13:21:58 +0200

    style: format code for better readability and consistency

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,25 +1,27 @@
 function countStudents(path) {
-    const fs = require('fs');
-    fs.readFile(path, 'utf-8', (err, data) => {
-        if (err) {
-            throw new Error('Cannot load the database');
+  const fs = require("fs");
+  fs.readFile(path, "utf-8", (err, data) => {
+    if (err) {
+      throw new Error("Cannot load the database");
+    }
+    const lines = data.split("\n").slice(1);
+    const students = {};
+    lines.forEach((line) => {
+      if (line) {
+        const [firstName, lastName, age, field] = line.split(",");
+        if (!students[field]) {
+          students[field] = [];
         }
-        const lines = data.split('\n').slice(1);
-        const students = {};
-        lines.forEach(line => {
-            if (line) {
-                const [firstName, lastName, age, field] = line.split(',');
-                if (!students[field]) {
-                    students[field] = [];
-                }
-                students[field].push(firstName);
-            }
-        });
-        const totalStudents = lines.filter(line => line).length;
-        console.log(`Number of students: ${totalStudents}`);
-        for (const field in students) {
-            console.log(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
-        }
+        students[field].push(firstName);
+      }
     });
+    const totalStudents = lines.filter((line) => line).length;
+    console.log(`Number of students: ${totalStudents}`);
+    for (const field in students) {
+      console.log(
+        `Number of students in ${field}: ${students[field].length}. List: ${students[field].join(", ")}`,
+      );
+    }
+  });
 }
 module.exports = countStudents;
