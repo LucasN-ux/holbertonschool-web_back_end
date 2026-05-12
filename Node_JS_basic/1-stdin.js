f8f79c94f582b7727a60cb25850bf48091492a5b
LucasN-ux <lucas.n230896@gmail.com>
2026-05-12 13:10:56 +0200

    fix: replace console.log with process.stdout.write for consistent output

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,11 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
 process.stdin.setEncoding('utf-8');
+
 process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.toString().trim()}`);
+  process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
